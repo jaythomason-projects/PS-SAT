979a1978f8fc2f8e5a5552f9065c2d9cb8904548
Jay Thomason <jaythomason.projects@outlook.com>
2023-06-15 14:15:08 +1000

    feat: added New-XmlObjectFromXamlString removed old icon

--- modules/view.psm1
+++ modules/view.psm1
@@ -1,5 +1,14 @@
-function Convert-XamlStringToXmlObject {
-    # TODO: Create function
+function script:New-XmlObjectFromXamlString {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$XamlString
+    )
+
+    $stringReader = New-Object System.IO.StringReader($XamlString)
+    $xmlReader = [System.Xml.XmlReader]::Create($stringReader)
+    $xmlObject = [System.Windows.Markup.XamlReader]::Load($xmlReader)
+
+    return $xmlObject
 }
 
 function Add-TabItems {
